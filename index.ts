90f7e5c90e63da28d08439084e1558c998b9853a
wu_zemin <wu_zemin@hithinksoft.com>
2023-07-18 14:08:52 +0800

    feat: 支持v2模板

--- index.ts
+++ index.ts
@@ -1,29 +1,54 @@
-import * as process from 'node:process'
+import { cwd } from 'node:process'
 import { Command } from 'commander'
 import degit from 'degit'
+import { consola } from 'consola'
 import { version } from './package.json'
+
+const TEMPLATES = [
+  {
+    command: 'v3',
+    description: 'vue3',
+    git: 'https://github.com/lyric-zemin/vue3-vite-template.git',
+    options: { isDefault: true },
+  },
+  {
+    command: 'v2',
+    description: 'vue2',
+    git: 'https://github.com/lyric-zemin/vue2-vite-template.git',
+  },
+]
 
 const program = new Command()
 
 program
   .name('create-lyc')
-  .description('一个vue3初始代码命令')
+  .description('一个生成初始代码的命令')
   .version(version)
-  .argument('[dir]', '安装目录', process.cwd())
-  .action((dir) => {
-    const d = degit('https://github.com/lyric-zemin/vue3-vite-template.git', {
-      cache: true,
-      force: true,
-    })
 
-    d.on('info', (info) => {
-      console.log(info.message)
-    })
+for (const { command, description, git, options } of TEMPLATES) {
+  program.command(command, options)
+    .description(description)
+    .argument('[dir]', '安装目录', cwd())
+    .option('-f, --force', '覆盖目录文件')
+    .action((dir, { force }) => {
+      consola.start(`Command [${command}] start...`)
 
-    d.clone(dir).then(() => {
-      console.log('done')
+      const d = degit(git, {
+        force,
+        // cache: true,
+      })
+
+      d.on('info', (info) => {
+        consola.info(info.message)
+      })
+
+      d.clone(dir).then(() => {
+        consola.success('Project done!')
+      }).catch((err) => {
+        consola.error(err)
+      })
     })
-  })
+}
 
 program.showHelpAfterError('(添加 --help 展示更多信息)')
 
