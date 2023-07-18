7bb7f96ac9c3e76cc3d4b9d0076b3abf55a8ef4f
wu_zemin <wu_zemin@hithinksoft.com>
2023-07-18 14:37:51 +0800

    feat: 支持Vitesse模板

--- index.ts
+++ index.ts
@@ -1,20 +1,49 @@
 import { cwd } from 'node:process'
-import { Command } from 'commander'
+import { Command, type CommandOptions } from 'commander'
 import degit from 'degit'
 import { consola } from 'consola'
 import { version } from './package.json'
 
-const TEMPLATES = [
+interface Template {
+  /**
+   * 命令
+   */
+  command: string
+  /**
+   * 描述
+   */
+  description: string
+  /**
+   * git 地址
+   */
+  git: string
+  /**
+   * degit options
+   */
+  options?: CommandOptions
+}
+
+const TEMPLATES: Template[] = [
   {
     command: 'v3',
-    description: 'vue3',
+    description: 'vue3 初始模板',
     git: 'https://github.com/lyric-zemin/vue3-vite-template.git',
     options: { isDefault: true },
   },
   {
     command: 'v2',
-    description: 'vue2',
+    description: 'vue2 初始模板',
     git: 'https://github.com/lyric-zemin/vue2-vite-template.git',
+  },
+  {
+    command: 'vitesse',
+    description: 'vitesse',
+    git: 'https://github.com/antfu/vitesse.git',
+  },
+  {
+    command: 'vitesse-lite',
+    description: '轻量版的 vitesse ',
+    git: 'https://github.com/antfu/vitesse-lite.git',
   },
 ]
 
@@ -30,13 +59,11 @@ for (const { command, description, git, options } of TEMPLATES) {
     .description(description)
     .argument('[dir]', '安装目录', cwd())
     .option('-f, --force', '覆盖目录文件')
-    .action((dir, { force }) => {
+    .option('--cache', '使用缓存版本')
+    .action((dir, { force, cache }) => {
       consola.start(`Command [${command}] start...`)
 
-      const d = degit(git, {
-        force,
-        // cache: true,
-      })
+      const d = degit(git, { force, cache })
 
       d.on('info', (info) => {
         consola.info(info.message)
